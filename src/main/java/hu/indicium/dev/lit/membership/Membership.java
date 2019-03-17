7f00495335d43c1fff728466761b681acc254d10
joostlek <joostlek@outlook.com>
2019-03-17 20:34:42 +0100

    Add Membership
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/lit/membership/Membership.java
@@ -0,0 +1,86 @@
+package hu.indicium.dev.lit.membership;
+
+import hu.indicium.dev.lit.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "memberships")
+public class Membership {
+    @Id
+    @SequenceGenerator(name = "member_id_generator", sequenceName = "member_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_id_generator")
+    private Long id;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    @CreationTimestamp
+    private Date created;
+
+    @UpdateTimestamp
+    private Date updated;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "fk_user")
+    private User user;
+
+    public Membership(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    protected void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- src/main/java/hu/indicium/dev/lit/user/User.java
+++ src/main/java/hu/indicium/dev/lit/user/User.java
@@ -1,6 +1,10 @@
 package hu.indicium.dev.lit.user;
 
+import hu.indicium.dev.lit.membership.Membership;
+
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -9,6 +13,9 @@ public class User {
     @SequenceGenerator(name = "user_id_generator", sequenceName = "user_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_generator")
     private Long id;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Membership> memberships = new HashSet<>();
 
     public User() {
     }
@@ -19,5 +26,17 @@ public class User {
 
     protected void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<Membership> getMemberships() {
+        return memberships;
+    }
+
+    public void setMemberships(Set<Membership> memberships) {
+        this.memberships = memberships;
+    }
+
+    public void addMembership(Membership membership) {
+        this.memberships.add(membership);
     }
 }
