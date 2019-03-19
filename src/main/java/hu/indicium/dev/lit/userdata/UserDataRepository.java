4a7e3f607e64559e2f6248aa33925f77d89188dd
joostlek <joostlek@outlook.com>
2019-03-19 21:41:35 +0100

    Add repository
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/lit/userdata/UserDataRepository.java
@@ -0,0 +1,6 @@
+package hu.indicium.dev.lit.userdata;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDataRepository extends JpaRepository<UserData, Long> {
+}
