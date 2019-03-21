a608736dcfbca8ec336fea5c516d110048bdeed2
joostlek <joostlek@outlook.com>
2019-03-21 14:25:35 +0100

    Add repository for StudyType
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/lit/study/StudyTypeRepository.java
@@ -0,0 +1,6 @@
+package hu.indicium.dev.lit.study;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyTypeRepository extends JpaRepository<StudyType, Long> {
+}
