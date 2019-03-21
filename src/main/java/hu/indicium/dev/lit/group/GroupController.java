34869ad76da19e57dc4e6cbf04502242da2bad00
joostlek <joostlek@outlook.com>
2019-03-21 11:05:24 +0100

    Add controller for groups
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/lit/group/GroupController.java
@@ -0,0 +1,66 @@
+package hu.indicium.dev.lit.group;
+
+import hu.indicium.dev.lit.group.dto.GroupDTO;
+import hu.indicium.dev.lit.group.dto.NewGroupDTO;
+import hu.indicium.dev.lit.response.Response;
+import hu.indicium.dev.lit.response.SuccessResponse;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
+
+@RestController
+public class GroupController {
+    private final GroupServiceInterface groupService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public GroupController(GroupServiceInterface groupService, ModelMapper modelMapper) {
+        this.groupService = groupService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/groups")
+    public Response getAllGroups() {
+        return new SuccessResponse(
+                groupService.getAllGroups()
+                        .stream()
+                        .map(this::convertToDTO)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @PostMapping("/groups")
+    public Response createNewGroup(@RequestBody NewGroupDTO newGroupDTO) {
+        return new SuccessResponse(
+                convertToDTO(groupService.createNewGroup(convertToEntity(newGroupDTO)))
+        );
+    }
+
+    @PutMapping("/groups/{groupId}")
+    public Response updateGroup(@RequestBody GroupDTO groupDTO, @PathVariable Long groupId) {
+        Group group = convertToEntity(groupDTO);
+        group.setId(groupId);
+        return new SuccessResponse(convertToDTO(groupService.updateGroup(group)));
+    }
+
+    @DeleteMapping("/groups/{groupId}")
+    public Response deleteGroup(@PathVariable Long groupId) {
+        groupService.deleteGroup(groupId);
+        return new SuccessResponse(null);
+    }
+
+    private GroupDTO convertToDTO(Group group) {
+        return modelMapper.map(group, GroupDTO.class);
+    }
+
+    private Group convertToEntity(NewGroupDTO groupDTO) {
+        return modelMapper.map(groupDTO, Group.class);
+    }
+
+    private Group convertToEntity(GroupDTO groupDTO) {
+        return modelMapper.map(groupDTO, Group.class);
+    }
+}
---
+++ src/main/java/hu/indicium/dev/lit/group/dto/NewGroupDTO.java
@@ -0,0 +1,13 @@
+package hu.indicium.dev.lit.group.dto;
+
+public class NewGroupDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
