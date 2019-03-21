6dfbd18e21dc73c8156d79023abc975b8c03137b
joostlek <joostlek@outlook.com>
2019-03-21 14:44:57 +0100

    Add StudyTypeController
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/lit/study/StudyTypeController.java
@@ -0,0 +1,66 @@
+package hu.indicium.dev.lit.study;
+
+import hu.indicium.dev.lit.response.DeleteSuccessResponse;
+import hu.indicium.dev.lit.response.Response;
+import hu.indicium.dev.lit.response.SuccessResponse;
+import hu.indicium.dev.lit.study.dto.NewStudyTypeDTO;
+import hu.indicium.dev.lit.study.dto.StudyTypeDTO;
+import hu.indicium.dev.lit.study.dto.UpdateStudyTypeDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
+
+@RestController
+public class StudyTypeController {
+
+    private final StudyTypeServiceInterface studyTypeService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public StudyTypeController(StudyTypeServiceInterface studyTypeService, ModelMapper modelMapper) {
+        this.studyTypeService = studyTypeService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/studytypes")
+    public Response getAllStudyTypes() {
+        return new SuccessResponse(studyTypeService.getAllStudyTypes()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList())
+        );
+    }
+
+    @PostMapping("/studytypes")
+    public Response createStudyType(@RequestBody NewStudyTypeDTO studyTypeDTO) {
+        return new SuccessResponse(studyTypeService.createStudyType(convertToEntity(studyTypeDTO)));
+    }
+
+    @PutMapping("/studytypes/{studyTypeId}")
+    public Response updateStudyType(@PathVariable Long studyTypeId, @RequestBody UpdateStudyTypeDTO studyTypeDTO) {
+        StudyType studyType = convertToEntity(studyTypeDTO);
+        studyType.setId(studyTypeId);
+        return new SuccessResponse(convertToDTO(studyTypeService.updateStudyType(studyType)));
+    }
+
+    @DeleteMapping("/studytypes/{studyTypeId}")
+    public Response deleteStudyTypes(@PathVariable Long studyTypeId) {
+        studyTypeService.deleteStudyType(studyTypeId);
+        return new DeleteSuccessResponse();
+    }
+
+    private StudyTypeDTO convertToDTO(StudyType studyType) {
+        return modelMapper.map(studyType, StudyTypeDTO.class);
+    }
+
+    private StudyType convertToEntity(NewStudyTypeDTO studyTypeDTO) {
+        return modelMapper.map(studyTypeDTO, StudyType.class);
+    }
+
+    private StudyType convertToEntity(UpdateStudyTypeDTO studyTypeDTO) {
+        return modelMapper.map(studyTypeDTO, StudyType.class);
+    }
+}
---
+++ src/main/java/hu/indicium/dev/lit/study/dto/StudyTypeDTO.java
@@ -0,0 +1,31 @@
+package hu.indicium.dev.lit.study.dto;
+
+public class StudyTypeDTO {
+    private Long id;
+    private String shortName;
+    private String longName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+}
