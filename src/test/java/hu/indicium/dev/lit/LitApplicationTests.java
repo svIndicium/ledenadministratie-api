178c9126b32e3bcfbaaaeb21d484bd757e3394e7
joostlek <joostlek@outlook.com>
2019-03-21 16:45:23 +0100

    Prepare for testing
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/test/java/hu/indicium/dev/lit/LitApplicationTests.java
+++ src/test/java/hu/indicium/dev/lit/LitApplicationTests.java
@@ -1,16 +1,30 @@
 package hu.indicium.dev.lit;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class LitApplicationTests {
+@DisplayName("Application")
+@Tag("Application")
+class LitApplicationTests {
 
     @Test
-    public void contextLoads() {
+    @DisplayName("Test context loading")
+    void contextLoads() {
+    }
+
+    @Test
+    @DisplayName("Test startup")
+    void main() {
+        LitApplication.main(new String[]{});
+        assertThat(true).isTrue();
     }
 
 }
