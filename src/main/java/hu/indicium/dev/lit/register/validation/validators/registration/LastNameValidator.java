80538d610a52ee38147280222f9bde4e6fc2f6e8
joostlek <joostlek@outlook.com>
2019-08-18 21:13:51 +0200

    Add Last name validator
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/lit/register/validation/validators/registration/LastNameValidator.java
+++ src/main/java/hu/indicium/dev/lit/register/validation/validators/registration/LastNameValidator.java
@@ -4,8 +4,13 @@ import hu.indicium.dev.lit.register.Registration;
 import hu.indicium.dev.lit.util.Validator;
 
 public class LastNameValidator implements Validator<Registration> {
+
+    public static final String ERROR_MESSAGE = "Last name invalid";
+
     @Override
     public void validate(Registration validationEntity) {
-
+        if (validationEntity.getLastName() == null || validationEntity.getLastName().equals("")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
