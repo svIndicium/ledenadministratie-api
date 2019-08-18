1004b1f3244db4ec282e5b9b9341312aff7141e8
joostlek <joostlek@outlook.com>
2019-08-18 20:02:29 +0200

    Finish first name validator
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/lit/register/validation/validators/registration/FirstNameValidator.java
+++ src/main/java/hu/indicium/dev/lit/register/validation/validators/registration/FirstNameValidator.java
@@ -3,9 +3,23 @@ package hu.indicium.dev.lit.register.validation.validators.registration;
 import hu.indicium.dev.lit.register.Registration;
 import hu.indicium.dev.lit.util.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FirstNameValidator implements Validator<Registration> {
+
+    public static String ERROR_MESSAGE = "First name invalid";
+
     @Override
     public void validate(Registration validationEntity) {
-
+        if (validationEntity.getFirstName() == null || validationEntity.getFirstName().equals("")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        String firstName = validationEntity.getFirstName();
+        Pattern pattern = Pattern.compile("^(?=.{1,40}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$");
+        Matcher matcher = pattern.matcher(firstName);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
