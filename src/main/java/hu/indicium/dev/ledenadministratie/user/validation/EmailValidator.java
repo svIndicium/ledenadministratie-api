c35ab6bbb84ec26b28a4d089468ef790f206fcd4
joostlek <joostlek@outlook.com>
2019-09-13 12:07:21 +0200

    Add test for email validation
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/user/validation/EmailValidator.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/user/validation/EmailValidator.java
@@ -5,8 +5,13 @@ import hu.indicium.dev.ledenadministratie.util.Validator;
 
 public class EmailValidator implements Validator<User> {
 
+    public final static String EMPTY_EMAIL_ERROR_MESSAGE = "Empty email supplied";
+
     @Override
     public void validate(User user) {
+        if (user.getEmail().isBlank()) {
+            throw new IllegalArgumentException(EMPTY_EMAIL_ERROR_MESSAGE);
+        }
         if (!user.getEmail().contains("@")) {
             throw new IllegalArgumentException("Email incorrect");
         }
---
+++ src/test/java/hu/indicium/dev/ledenadministratie/user/validation/EmailValidatorTest.java
@@ -0,0 +1,53 @@
+package hu.indicium.dev.ledenadministratie.user.validation;
+
+import hu.indicium.dev.ledenadministratie.studytype.StudyType;
+import hu.indicium.dev.ledenadministratie.user.User;
+import hu.indicium.dev.ledenadministratie.util.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName("User email validator")
+class EmailValidatorTest {
+
+    private Validator<User> emailValidator = new EmailValidator();
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setPhoneNumber("+31612345678");
+        user.setDateOfBirth(new Date());
+        user.setStudyType(new StudyType());
+    }
+
+    @Test
+    @DisplayName("Validate")
+    void validate() {
+        user.setEmail("john@doe.com");
+        emailValidator.validate(user);
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Validate empty email address")
+    void validateEmptyEmailShouldThrowException() {
+        user.setEmail("");
+        try {
+            emailValidator.validate(user);
+            fail();
+        } catch (Exception e) {
+            assertThat(e.getMessage()).isEqualTo(EmailValidator.EMPTY_EMAIL_ERROR_MESSAGE);
+        }
+    }
+}
