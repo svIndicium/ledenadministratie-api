51fba67fc883eea465ac7db1c53929b018832775
joostlek <joostlek@outlook.com>
2019-09-13 11:33:47 +0200

    Add method for reading users
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/user/UserService.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/user/UserService.java
@@ -2,9 +2,15 @@ package hu.indicium.dev.ledenadministratie.user;
 
 import hu.indicium.dev.ledenadministratie.user.dto.UserDTO;
 
+import java.util.List;
+
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
 
     UserDTO editUser(UserDTO userDTO);
+
+    UserDTO getUserById(Long userId);
+
+    List<UserDTO> getUsers();
 }
 
--- src/main/java/hu/indicium/dev/ledenadministratie/user/UserServiceImpl.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/user/UserServiceImpl.java
@@ -5,6 +5,10 @@ import hu.indicium.dev.ledenadministratie.user.dto.UserDTO;
 import hu.indicium.dev.ledenadministratie.util.Validator;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -36,6 +40,24 @@ public class UserServiceImpl implements UserService {
         user.setStudyType(StudyTypeMapper.toEntity(userDTO.getStudyType()));
         user = this.saveUser(user);
         return UserMapper.toDTO(user);
+    }
+
+    @Override
+    public UserDTO getUserById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User " + userId + " not found!"));
+        return UserMapper.toDTO(user);
+    }
+
+    @Override
+    public List<UserDTO> getUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserDTO> userDTOS = new ArrayList<>();
+        for (User user : users) {
+            UserDTO userDTO = UserMapper.toDTO(user);
+            userDTOS.add(userDTO);
+        }
+        return userDTOS;
     }
 
     private User saveUser(User user) {
