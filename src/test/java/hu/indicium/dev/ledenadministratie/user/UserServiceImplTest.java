3c9f4ff0bc09461e56ae97707e91dcde862c694e
joostlek <joostlek@outlook.com>
2019-09-26 08:25:28 +0200

    Update tests
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/test/java/hu/indicium/dev/ledenadministratie/user/UserServiceImplTest.java
+++ src/test/java/hu/indicium/dev/ledenadministratie/user/UserServiceImplTest.java
@@ -1,5 +1,7 @@
 package hu.indicium.dev.ledenadministratie.user;
 
+import hu.indicium.dev.ledenadministratie.hooks.CreationHook;
+import hu.indicium.dev.ledenadministratie.hooks.UpdateHook;
 import hu.indicium.dev.ledenadministratie.studytype.StudyType;
 import hu.indicium.dev.ledenadministratie.studytype.dto.StudyTypeDTO;
 import hu.indicium.dev.ledenadministratie.user.dto.UserDTO;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -16,7 +19,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -38,6 +43,15 @@ class UserServiceImplTest {
     @MockBean
     private Validator<User> userValidator;
 
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @MockBean
+    private CreationHook<UserDTO> creationHook;
+
+    @MockBean
+    private UpdateHook<UserDTO> updateHook;
+
     @Autowired
     private UserService userService;
 
@@ -56,6 +70,7 @@ class UserServiceImplTest {
         user.setLastName("Doe");
         user.setEmail("John@doe.com");
         user.setStudyType(studyType);
+        user.setToReceiveNewsletter(false);
         user.setDateOfBirth(new Date());
 
         UserDTO userDTO = new UserDTO();
@@ -64,6 +79,7 @@ class UserServiceImplTest {
         userDTO.setLastName(user.getLastName());
         userDTO.setEmail(user.getEmail());
         userDTO.setStudyType(studyTypeDTO);
+        userDTO.setToReceiveNewsletter(user.isToReceiveNewsletter());
         userDTO.setDateOfBirth(user.getDateOfBirth());
 
         when(userMapper.toDTO(any(User.class))).thenReturn(userDTO);
@@ -74,6 +90,8 @@ class UserServiceImplTest {
 
         verify(userRepository).save(eq(user));
         verify(userValidator, atLeastOnce()).validate(any(User.class));
+        verify(creationHook, times(1)).execute(null, userDTO);
+        verify(updateHook, never()).execute(any(UserDTO.class), any(UserDTO.class));
 
         assertThat(returnedUserDTO).isNotNull();
     }
@@ -93,6 +111,7 @@ class UserServiceImplTest {
         user.setLastName("Doe");
         user.setEmail("John@doe.com");
         user.setStudyType(studyType);
+        user.setToReceiveNewsletter(true);
         user.setDateOfBirth(new Date());
 
         UserDTO userDTO = new UserDTO();
@@ -101,6 +120,7 @@ class UserServiceImplTest {
         userDTO.setLastName(user.getLastName());
         userDTO.setEmail(user.getEmail());
         userDTO.setStudyType(studyTypeDTO);
+        userDTO.setToReceiveNewsletter(user.isToReceiveNewsletter());
         userDTO.setDateOfBirth(user.getDateOfBirth());
 
         when(userMapper.toDTO(any(User.class))).thenReturn(userDTO);
@@ -120,8 +140,62 @@ class UserServiceImplTest {
 
         verify(userValidator, atLeastOnce()).validate(any(User.class));
         verify(userRepository, never()).save(eq(user));
+        verify(creationHook, never()).execute(any(UserDTO.class), any(UserDTO.class));
+        verify(updateHook, never()).execute(any(UserDTO.class), any(UserDTO.class));
 
         assertThat(returnedUserDTO).isNull();
+    }
+
+    @Test
+    @DisplayName("Get user by id")
+    void getUserById() {
+        StudyType studyType = new StudyType("Software Development");
+        studyType.setId(1L);
+        StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
+        studyTypeDTO.setId(studyType.getId());
+        studyTypeDTO.setName(studyType.getName());
+
+        User user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setMiddleName("Daniel");
+        user.setLastName("Doe");
+        user.setEmail("John@doe.com");
+        user.setStudyType(studyType);
+        user.setToReceiveNewsletter(true);
+        user.setDateOfBirth(new Date());
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setMiddleName(user.getMiddleName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setStudyType(studyTypeDTO);
+        userDTO.setToReceiveNewsletter(user.isToReceiveNewsletter());
+        userDTO.setDateOfBirth(user.getDateOfBirth());
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(user));
+        when(userMapper.toDTO(any(User.class))).thenReturn(userDTO);
+
+        UserDTO receivedUser = userService.getUserById(1L);
+
+        assertThat(receivedUser).isEqualTo(userDTO);
+    }
+
+    @Test
+    @DisplayName("Throw exception when getting non-existing user")
+    void shouldThrowException_whenGetNonExistingUserById() {
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        try {
+            UserDTO receivedUser = userService.getUserById(1L);
+            fail();
+        } catch (Exception ex) {
+            assertThat(ex.getClass()).isEqualTo(EntityNotFoundException.class);
+            assertThat(ex.getMessage()).isEqualTo("User 1 not found!");
+        }
+
+
     }
 
     @TestConfiguration
@@ -136,9 +210,18 @@ class UserServiceImplTest {
         @Autowired
         private Validator<User> userValidator;
 
+        @Autowired
+        private ModelMapper modelMapper;
+
+        @Autowired
+        private CreationHook<UserDTO> creationHook;
+
+        @Autowired
+        private UpdateHook<UserDTO> updateHook;
+
         @Bean
         public UserService userService() {
-            return new UserServiceImpl(userRepository, userValidator, userMapper);
+            return new UserServiceImpl(userRepository, userValidator, userMapper, modelMapper, creationHook, updateHook);
         }
     }
 }
