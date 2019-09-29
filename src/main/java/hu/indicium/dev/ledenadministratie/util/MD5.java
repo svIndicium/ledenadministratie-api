e8367d621e1c2df7cd94c9ba0244559006ec1619
joostlek <joostlek@outlook.com>
2019-09-29 20:01:34 +0200

    Use logger for exception that will never happen
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/util/MD5.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/util/MD5.java
@@ -1,17 +1,23 @@
 package hu.indicium.dev.ledenadministratie.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5 {
+
+    private Logger logger = LoggerFactory.getLogger(MD5.class);
+
     public String hash(String string) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
             byte[] digest = messageDigest.digest(string.getBytes());
             return DatatypeConverter.printHexBinary(digest);
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
             return "";
         }
     }
