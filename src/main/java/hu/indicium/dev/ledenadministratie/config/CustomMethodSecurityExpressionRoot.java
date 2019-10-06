83476acd4909a216fb54c728ab03ad81b7258b27
joostlek <joostlek@outlook.com>
2019-10-06 17:51:02 +0200

    Add Auth0 Method Security
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/hu/indicium/dev/ledenadministratie/config/CustomMethodSecurityExpressionRoot.java
@@ -0,0 +1,46 @@
+package hu.indicium.dev.ledenadministratie.config;
+
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionOperations;
+import org.springframework.security.core.Authentication;
+
+public class CustomMethodSecurityExpressionRoot extends SecurityExpressionRoot implements MethodSecurityExpressionOperations {
+
+    Object filterObject;
+
+    Object returnObject;
+
+    /**
+     * Creates a new instance
+     *
+     * @param authentication the {@link Authentication} to use. Cannot be null.
+     */
+    public CustomMethodSecurityExpressionRoot(Authentication authentication) {
+        super(authentication);
+    }
+
+    @Override
+    public void setFilterObject(Object filterObject) {
+        this.filterObject = filterObject;
+    }
+
+    @Override
+    public Object getFilterObject() {
+        return this.filterObject;
+    }
+
+    @Override
+    public void setReturnObject(Object returnObject) {
+        this.returnObject = returnObject;
+    }
+
+    @Override
+    public Object getReturnObject() {
+        return this.returnObject;
+    }
+
+    @Override
+    public Object getThis() {
+        return this;
+    }
+}
--- src/main/java/hu/indicium/dev/ledenadministratie/config/RestTemplateConfig.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/config/RestTemplateConfig.java
@@ -1,13 +1,19 @@
 package hu.indicium.dev.ledenadministratie.config;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter jsonHttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        jsonHttpMessageConverter.getObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        restTemplate.getMessageConverters().add(jsonHttpMessageConverter);
+        return restTemplate;
     }
 }
