d682e6eb48291e5cee892eb368577b9ee5d2942a
joostlek <joostlek@outlook.com>
2019-10-20 20:09:06 +0200

    Add tests
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/test/java/hu/indicium/dev/ledenadministratie/studytype/StudyTypeServiceImplTest.java
+++ src/test/java/hu/indicium/dev/ledenadministratie/studytype/StudyTypeServiceImplTest.java
@@ -13,13 +13,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -67,6 +69,95 @@ class StudyTypeServiceImplTest {
             fail();
         } catch (EntityNotFoundException e) {
             assertThat(e.getMessage()).isEqualTo("Studytype 1 not found");
+        }
+    }
+
+    @Test
+    @DisplayName("Study type with the same name exists")
+    void shouldReturnTrue_whenRepositorySaysItsTrue() {
+        when(studyTypeRepository.existsByName(eq("SD"))).thenReturn(true);
+
+        assertThat(studyTypeService.isNameInUse("SD")).isTrue();
+    }
+
+    @Test
+    @DisplayName("Study type with the same name does not exist")
+    void shouldReturnFalse_whenRepositorySaysItsFalse() {
+        when(studyTypeRepository.existsByName(eq("SD"))).thenReturn(false);
+
+        assertThat(studyTypeService.isNameInUse("SD")).isFalse();
+    }
+
+    @Test
+    @DisplayName("Get all study types")
+    void shouldReturnAListOfDTOs_whenGetAllStudyTypes() {
+        StudyType studyType = new StudyType("SD");
+        studyType.setId(1L);
+
+        StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
+        studyType.setId(studyType.getId());
+        studyType.setName(studyType.getName());
+
+        StudyType studyType1 = new StudyType("TI");
+        studyType1.setId(2L);
+
+        StudyTypeDTO studyTypeDTO1 = new StudyTypeDTO();
+        studyType1.setId(studyType.getId());
+        studyType1.setName(studyType.getName());
+
+        when(studyTypeMapper.toDTO(eq(studyType))).thenReturn(studyTypeDTO);
+        when(studyTypeMapper.toDTO(eq(studyType1))).thenReturn(studyTypeDTO1);
+
+        when(studyTypeRepository.findAll()).thenReturn(Arrays.asList(studyType, studyType1));
+
+        List<StudyTypeDTO> studyTypeDTOS = studyTypeService.getAllStudyTypes();
+
+        assertThat(studyTypeDTOS).hasSize(2);
+        assertThat(studyTypeDTOS.get(0)).isEqualToComparingFieldByField(studyTypeDTO);
+        assertThat(studyTypeDTOS.get(1)).isEqualToComparingFieldByField(studyTypeDTO1);
+    }
+
+    @Test
+    @DisplayName("Create new study type")
+    void createStudyType() {
+        StudyType studyType = new StudyType("SD");
+        studyType.setId(1L);
+
+        StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
+        studyTypeDTO.setId(studyType.getId());
+        studyTypeDTO.setName(studyType.getName());
+
+        when(studyTypeMapper.toEntity(any(StudyTypeDTO.class))).thenReturn(studyType);
+        when(studyTypeMapper.toDTO(any(StudyType.class))).thenReturn(studyTypeDTO);
+        when(studyTypeRepository.save(any(StudyType.class))).thenReturn(studyType);
+
+        StudyTypeDTO result = studyTypeService.createStudyType(studyTypeDTO);
+
+        verify(studyTypeValidator, times(1)).validate(any(StudyType.class));
+        verify(studyTypeRepository, times(1)).save(any(StudyType.class));
+
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Validation error should stop creating user")
+    void shouldNotCreateUser_whenValidationIsThrowingAnException() {
+        StudyType studyType = new StudyType("SD");
+        studyType.setId(1L);
+
+        StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
+        studyTypeDTO.setId(studyType.getId());
+        studyTypeDTO.setName(studyType.getName());
+
+        when(studyTypeMapper.toEntity(any(StudyTypeDTO.class))).thenReturn(studyType);
+        when(studyTypeRepository.save(any(StudyType.class))).thenReturn(studyType);
+        doThrow(new IllegalArgumentException("Exception")).when(studyTypeValidator).validate(any(StudyType.class));
+
+        try {
+            StudyTypeDTO result = studyTypeService.createStudyType(studyTypeDTO);
+            fail();
+        } catch (Exception e) {
+            verify(studyTypeRepository, never()).save(any(StudyType.class));
         }
     }
 
