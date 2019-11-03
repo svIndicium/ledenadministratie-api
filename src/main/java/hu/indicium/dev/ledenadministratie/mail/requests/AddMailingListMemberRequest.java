775d3ecb57359e8bfbc05a9cff3dec89d1bed979
joostlek <joostlek@outlook.com>
2019-11-03 20:16:19 +0100

    Fix mailchimp integration not working
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/mail/requests/AddMailingListMemberRequest.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/mail/requests/AddMailingListMemberRequest.java
@@ -1,28 +1,65 @@
 package hu.indicium.dev.ledenadministratie.mail.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import hu.indicium.dev.ledenadministratie.mail.dto.MailEntryDTO;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AddMailingListMemberRequest {
 
-    private String email_address;
+    @JsonProperty("email_address")
+    private String emailAddress;
 
+    @JsonProperty("status")
     private String status;
 
+    @JsonProperty("tags")
     private List<String> tags;
 
-    private Map<String, String> merge_fields;
+    @JsonProperty("merge_fields")
+    private Map<String, String> mergeFields;
 
     public AddMailingListMemberRequest(MailEntryDTO mailEntryDTO) {
-        this.email_address = mailEntryDTO.getEmail();
+        this.emailAddress = mailEntryDTO.getEmail();
         this.status = mailEntryDTO.isToReceiveNewsletter() ? "subscribed" : "unsubscribed";
-        this.tags = Collections.singletonList("new");
-        this.merge_fields = new HashMap<>();
-        this.merge_fields.put("FNAME", mailEntryDTO.getFirstName());
-        this.merge_fields.put("LNAME", mailEntryDTO.getLastName());
+        this.tags = Arrays.asList("new", "newnew");
+        this.mergeFields = new HashMap<>();
+        this.mergeFields.put("FNAME", mailEntryDTO.getFirstName());
+        this.mergeFields.put("LNAME", mailEntryDTO.getLastName());
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Map<String, String> getMergeFields() {
+        return mergeFields;
+    }
+
+    public void setMergeFields(Map<String, String> mergeFields) {
+        this.mergeFields = mergeFields;
     }
 }
---
+++ src/test/java/hu/indicium/dev/ledenadministratie/registration/RegistrationMapperTest.java
@@ -0,0 +1,13 @@
+package hu.indicium.dev.ledenadministratie.registration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith({SpringExtension.class})
+@DisplayName("Registration Mapper")
+class RegistrationMapperTest {
+
+}
