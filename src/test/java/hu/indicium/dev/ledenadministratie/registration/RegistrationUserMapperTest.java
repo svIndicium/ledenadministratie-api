35f8cdeebbcd703cc236d38957463e94cac63611
joostlek <joostlek@outlook.com>
2019-11-27 15:00:51 +0100

    Add test for user to registration mapper
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/test/java/hu/indicium/dev/ledenadministratie/registration/RegistrationUserMapperTest.java
+++ src/test/java/hu/indicium/dev/ledenadministratie/registration/RegistrationUserMapperTest.java
@@ -34,6 +34,71 @@ class RegistrationUserMapperTest {
         StudyType studyType = new StudyType("Software Development");
         studyType.setId(1L);
 
+        RegistrationDTO registrationDTO = getRegistrationDTO();
+
+        UserDTO userDTO = registrationUserMapper.toDTO(registrationDTO);
+
+        assertThat(userDTO).isNotNull();
+        assertThat(userDTO.getId()).isNull();
+        assertThat(userDTO.getFirstName()).isEqualTo(registrationDTO.getFirstName());
+        assertThat(userDTO.getMiddleName()).isEqualTo(registrationDTO.getMiddleName());
+        assertThat(userDTO.getLastName()).isEqualTo(registrationDTO.getLastName());
+        assertThat(userDTO.getEmail()).isEqualTo(registrationDTO.getEmail());
+        assertThat(userDTO.getPhoneNumber()).isEqualTo(registrationDTO.getPhoneNumber());
+        assertThat(userDTO.isToReceiveNewsletter()).isEqualTo(registrationDTO.isToReceiveNewsletter());
+        assertThat(userDTO.getDateOfBirth()).isEqualTo(registrationDTO.getDateOfBirth());
+        assertThat(userDTO.getStudyType()).isNotNull();
+        assertThat(userDTO.getStudyType()).isEqualToComparingFieldByField(studyType);
+    }
+
+    @Test
+    @DisplayName("Convert UserDTO to Registration")
+    void shouldReturnCorrectRegistration_whenConvertUserDTO() {
+        StudyType studyType = new StudyType("Software Development");
+        studyType.setId(1L);
+
+        UserDTO userDTO = getUserDTO();
+
+        RegistrationDTO registrationDTO = registrationUserMapper.toEntity(userDTO);
+
+        assertThat(registrationDTO).isNotNull();
+        assertThat(registrationDTO.getId()).isEqualTo(userDTO.getId());
+        assertThat(registrationDTO.getFirstName()).isEqualTo(userDTO.getFirstName());
+        assertThat(registrationDTO.getMiddleName()).isEqualTo(userDTO.getMiddleName());
+        assertThat(registrationDTO.getLastName()).isEqualTo(userDTO.getLastName());
+        assertThat(registrationDTO.getEmail()).isEqualTo(userDTO.getEmail());
+        assertThat(registrationDTO.getPhoneNumber()).isEqualTo(userDTO.getPhoneNumber());
+        assertThat(registrationDTO.isToReceiveNewsletter()).isEqualTo(userDTO.isToReceiveNewsletter());
+        assertThat(registrationDTO.getDateOfBirth()).isEqualTo(userDTO.getDateOfBirth());
+        assertThat(registrationDTO.getStudyType()).isNotNull();
+        assertThat(registrationDTO.getStudyType()).isEqualToComparingFieldByField(studyType);
+    }
+
+    private UserDTO getUserDTO() {
+        StudyType studyType = new StudyType("Software Development");
+        studyType.setId(1L);
+
+        StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
+        studyTypeDTO.setName(studyType.getName());
+        studyTypeDTO.setId(studyType.getId());
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setFirstName("John");
+        userDTO.setMiddleName("Nicholas");
+        userDTO.setLastName("Doe");
+        userDTO.setEmail("John@doe.com");
+        userDTO.setDateOfBirth(new Date());
+        userDTO.setPhoneNumber("+31612345678");
+        userDTO.setToReceiveNewsletter(true);
+        userDTO.setStudyType(studyTypeDTO);
+        return userDTO;
+    }
+
+    private RegistrationDTO getRegistrationDTO() {
+        StudyType studyType = new StudyType("Software Development");
+        studyType.setId(1L);
+
         StudyTypeDTO studyTypeDTO = new StudyTypeDTO();
         studyTypeDTO.setName(studyType.getName());
         studyTypeDTO.setId(studyType.getId());
@@ -52,20 +117,7 @@ class RegistrationUserMapperTest {
         registrationDTO.setFinalizedBy("Rick Astley");
         registrationDTO.setFinalizedAt(new Date());
         registrationDTO.setComment("Never let us down!");
-
-        UserDTO userDTO = registrationUserMapper.toDTO(registrationDTO);
-
-        assertThat(userDTO).isNotNull();
-        assertThat(userDTO.getId()).isNull();
-        assertThat(userDTO.getFirstName()).isEqualTo(registrationDTO.getFirstName());
-        assertThat(userDTO.getMiddleName()).isEqualTo(registrationDTO.getMiddleName());
-        assertThat(userDTO.getLastName()).isEqualTo(registrationDTO.getLastName());
-        assertThat(userDTO.getEmail()).isEqualTo(registrationDTO.getEmail());
-        assertThat(userDTO.getPhoneNumber()).isEqualTo(registrationDTO.getPhoneNumber());
-        assertThat(userDTO.isToReceiveNewsletter()).isEqualTo(registrationDTO.isToReceiveNewsletter());
-        assertThat(userDTO.getDateOfBirth()).isEqualTo(registrationDTO.getDateOfBirth());
-        assertThat(userDTO.getStudyType()).isNotNull();
-        assertThat(userDTO.getStudyType()).isEqualToComparingFieldByField(studyType);
+        return registrationDTO;
     }
 
     @TestConfiguration
