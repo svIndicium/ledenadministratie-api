b015edbc95ff8e20a75dd8480d8a04d2b49b8a05
joostlek <joostlek@outlook.com>
2019-12-03 15:43:30 +0100

    Add documentation to user controller
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/user/UserController.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/user/UserController.java
@@ -4,6 +4,8 @@ import hu.indicium.dev.ledenadministratie.user.dto.UserDTO;
 import hu.indicium.dev.ledenadministratie.user.requests.CreateUserRequest;
 import hu.indicium.dev.ledenadministratie.user.requests.UpdateUserRequest;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/user")
+@RequestMapping(value = "/user")
 @Api(tags = "User Endpoint", value = "Users")
 public class UserController {
 
@@ -25,27 +27,38 @@ public class UserController {
         userRequestMapper = new UserRequestMapper();
     }
 
+    @ApiOperation(value = "Create a new user", response = UserDTO.class)
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
+    public UserDTO createUser(
+            @ApiParam(value = "User data to store", required = true) @RequestBody @Valid CreateUserRequest createUserRequest
+    ) {
         UserDTO userDTO = userRequestMapper.toDTO(createUserRequest);
         return userService.createUser(userDTO);
     }
 
+    @ApiOperation(value = "Update a user", response = UserDTO.class)
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO updateUser(@RequestBody @Valid UpdateUserRequest updateUserRequest, @PathVariable("id") Long userId) {
+    public UserDTO updateUser(
+            @ApiParam(value = "User data to update user") @RequestBody @Valid UpdateUserRequest updateUserRequest,
+            @ApiParam(value = "Id of the to be updated user", required = true) @PathVariable("id") Long userId
+    ) {
         UserDTO userDTO = userRequestMapper.toDTO(updateUserRequest);
         userDTO.setId(userId);
         return userService.updateUser(userDTO);
     }
 
+    @ApiOperation(value = "Get user by id", response = UserDTO.class)
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO getUser(@PathVariable("id") Long userId) {
+    public UserDTO getUser(
+            @ApiParam(value = "Id of the requested user", required = true) @PathVariable("id") Long userId
+    ) {
         return userService.getUserById(userId);
     }
 
+    @ApiOperation(value = "Get a list of all users", response = List.class)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<UserDTO> getUsers() {
