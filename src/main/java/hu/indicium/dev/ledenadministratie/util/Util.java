f3803bce712693b3fd9e8c439c80adba9ec8fe3a
joostlek <joostlek@outlook.com>
2020-04-08 02:51:57 +0200

    Fix random bug
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/util/Util.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/util/Util.java
@@ -1,5 +1,7 @@
 package hu.indicium.dev.ledenadministratie.util;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class Util {
@@ -18,7 +20,13 @@ public class Util {
 
     public static String randomAlphaNumeric(int count) {
         StringBuilder builder = new StringBuilder();
-        Random random = new Random();
+        Random random;
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
         while (count-- != 0) {
             int character = random.nextInt() * ALPHA_NUMERIC_STRING.length();
             builder.append(ALPHA_NUMERIC_STRING.charAt(character));
