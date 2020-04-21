96396d93484a172225eaae72b1856185a2699932
joostlek <joostlek@outlook.com>
2020-04-21 11:27:34 +0200

    Add error message to empty value
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/setting/requests/UpdateSettingRequest.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/setting/requests/UpdateSettingRequest.java
@@ -3,7 +3,7 @@ package hu.indicium.dev.ledenadministratie.setting.requests;
 import javax.validation.constraints.NotBlank;
 
 public class UpdateSettingRequest {
-    @NotBlank
+    @NotBlank(message = "Waarde mag niet leeg zijn")
     private String value;
 
     public String getValue() {
