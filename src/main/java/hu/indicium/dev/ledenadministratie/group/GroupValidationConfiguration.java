6fdace655e8d4d960fe735eaa85c7cab20522059
joostlek <joostlek@outlook.com>
2020-05-26 16:40:08 +0200

    Add lazy annotation
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/group/GroupValidationConfiguration.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/group/GroupValidationConfiguration.java
@@ -4,6 +4,7 @@ import hu.indicium.dev.ledenadministratie.group.validation.UniqueGroupNameValida
 import hu.indicium.dev.ledenadministratie.util.ValidatorGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ public class GroupValidationConfiguration {
 
     private final GroupService groupService;
 
-    public GroupValidationConfiguration(GroupService groupService) {
+    public GroupValidationConfiguration(@Lazy GroupService groupService) {
         this.groupService = groupService;
     }
 
