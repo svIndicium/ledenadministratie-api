8aacd71e97fd169b50ee2bf749ada6b1051f863d
joostlek <joostlek@outlook.com>
2020-10-16 20:25:08 +0200

    Remove obsolete code
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/membership/MembershipRepository.java
+++
@@ -1,8 +0,0 @@
-//package hu.indicium.dev.ledenadministratie.membership;
-//
-//import hu.indicium.dev.ledenadministratie.domain.model.user.member.membership.Membership;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MembershipRepository extends JpaRepository<Membership, Long> {
-//
-//}
--- src/main/java/hu/indicium/dev/ledenadministratie/studytype/StudyTypeRepository.java
+++
@@ -1,8 +0,0 @@
-//package hu.indicium.dev.ledenadministratie.studytype;
-//
-//import hu.indicium.dev.ledenadministratie.domain.model.studytype.StudyType;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface StudyTypeRepository extends JpaRepository<StudyType, Long> {
-//    boolean existsByName(String name);
-//}
