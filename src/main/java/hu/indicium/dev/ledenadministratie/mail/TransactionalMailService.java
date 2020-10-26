2eec262d1343d213b048b47ddc69b499595fb09e
joostlek <joostlek@outlook.com>
2020-10-26 08:50:41 +0100

    Remove old mail component
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/mail/TransactionalMailService.java
+++
@@ -1,9 +0,0 @@
-package hu.indicium.dev.ledenadministratie.mail;
-
-import hu.indicium.dev.ledenadministratie.mail.dto.TransactionalMailDTO;
-
-public interface TransactionalMailService {
-    void sendVerificationMail(TransactionalMailDTO transactionalMailDTO);
-
-    void sendPasswordResetMail(TransactionalMailDTO transactionalMailDTO);
-}
