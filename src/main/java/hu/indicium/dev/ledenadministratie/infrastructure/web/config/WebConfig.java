217e84e6ede39b96facab831899208734688785b
joostlek <joostlek@outlook.com>
2020-12-18 21:07:07 +0100

    Try to fix CORS
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/infrastructure/web/config/WebConfig.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/infrastructure/web/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true)
                 .allowedMethods("*")
-                .allowedOriginPatterns("*");
+                .allowedOrigins("*");
     }
 }
