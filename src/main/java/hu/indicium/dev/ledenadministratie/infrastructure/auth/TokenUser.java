1a3b2f18519bf124b8a0048a941a88e2631c8218
joostlek <joostlek@outlook.com>
2020-12-21 14:06:13 +0100

    Fix mapping to TokenUser
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/dev/ledenadministratie/infrastructure/auth/TokenUser.java
+++ src/main/java/hu/indicium/dev/ledenadministratie/infrastructure/auth/TokenUser.java
@@ -1,13 +1,14 @@
 package hu.indicium.dev.ledenadministratie.infrastructure.auth;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 public class TokenUser implements User {
 
     private final String name;
 
     public TokenUser(Authentication authentication) {
-        this.name = authentication.getName();
+        this.name = ((Jwt) authentication.getPrincipal()).getClaim("name");
     }
 
     @Override
