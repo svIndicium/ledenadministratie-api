8031a5af410873b4123e5e7bb68d22e92c84f862
joostlek <joostlek@outlook.com>
2021-04-20 17:16:35 +0200

    Update migration
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/resources/db/migration/V13__remove_verification_columns.sql
+++ src/main/resources/db/migration/V13__remove_verification_columns.sql
@@ -4,6 +4,4 @@ alter table mail_address drop column verified_at;
 
 alter table mail_address drop constraint uk_verification_token;
 
-drop index uk_verification_token;
-
 alter table mail_address drop column verification_token;
